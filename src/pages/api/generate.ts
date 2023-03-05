24724e075215d2f1505a5e3fc06184fe783217d4
yangyuezheng <yangyuezheng@bytedance.com>
2023-03-05 23:14:57 +0800

    feat: support openai key(multi) env

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -1,43 +1,55 @@
-import type { APIRoute } from 'astro'
-import { createParser, ParsedEvent, ReconnectInterval } from 'eventsource-parser'
+import type { APIRoute } from "astro";
+import {
+  createParser,
+  ParsedEvent,
+  ReconnectInterval,
+} from "eventsource-parser";
 
-const apiKey = import.meta.env.OPENAI_API_KEY
+// support multiple api keys( string split by comma)
+const apiKeyEnv = import.meta.env.OPENAI_API_KEY;
+
+function getRandomApiKey(apiKeysString: string): string {
+  const apiKeys = apiKeysString.split(",");
+  return apiKeys[Math.floor(Math.random() * apiKeys.length)];
+}
 
 export const post: APIRoute = async (context) => {
-  const body = await context.request.json()
-  const messages = body.messages
-  const encoder = new TextEncoder()
-  const decoder = new TextDecoder()
+  const apiKey = getRandomApiKey(apiKeyEnv);
+  const body = await context.request.json();
+  const messages = body.messages;
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
 
   if (!messages) {
-    return new Response('No input text')
+    return new Response("No input text");
   }
 
-  const completion = await fetch('https://api.openai.com/v1/chat/completions', {
+  const completion = await fetch("https://api.openai.com/v1/chat/completions", {
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
       Authorization: `Bearer ${apiKey}`,
     },
-    method: 'POST',
+    method: "POST",
     body: JSON.stringify({
-      model: 'gpt-3.5-turbo',
+      model: "gpt-3.5-turbo",
       messages,
       temperature: 0.6,
       stream: true,
     }),
-  })
+  });
 
   const stream = new ReadableStream({
     async start(controller) {
       // stream parser mode
       const streamParser = (event: ParsedEvent | ReconnectInterval) => {
-        if (event.type === 'event') {
-          const data = event.data
-          if (data === '[DONE]') {
-            controller.close()
-            return
+        if (event.type === "event") {
+          const data = event.data;
+          if (data === "[DONE]") {
+            controller.close();
+            return;
           }
           try {
+            // response 示例
             // response = {
             //   id: 'chatcmpl-6pULPSegWhFgi0XQ1DtgA3zTa1WR6',
             //   object: 'chat.completion.chunk',
@@ -47,22 +59,22 @@ export const post: APIRoute = async (context) => {
             //     { delta: { content: '你' }, index: 0, finish_reason: null }
             //   ],
             // }
-            const json = JSON.parse(data)
-            const text = json.choices[0].delta?.content            
-            const queue = encoder.encode(text)
-            controller.enqueue(queue)
+            const json = JSON.parse(data);
+            const text = json.choices[0].delta?.content;
+            const queue = encoder.encode(text);
+            controller.enqueue(queue);
           } catch (e) {
-            controller.error(e)
+            controller.error(e);
           }
         }
-      }
+      };
 
-      const parser = createParser(streamParser)
+      const parser = createParser(streamParser);
       for await (const chunk of completion.body as any) {
-        parser.feed(decoder.decode(chunk))
+        parser.feed(decoder.decode(chunk));
       }
     },
-  })
+  });
 
-  return new Response(stream)
-}
+  return new Response(stream);
+};
