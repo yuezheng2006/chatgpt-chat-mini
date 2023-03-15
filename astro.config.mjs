245043a731970fdfc0e3d2bc140a1c6dd1ac6c4f
yangyuezheng <yangyuezheng@bytedance.com>
2023-03-15 17:29:27 +0800

    feat: update astro vercel runtime setting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from "astro/config"
-import vercel from "@astrojs/vercel/edge"
-import unocss from "unocss/astro"
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+import unocss from "unocss/astro";
 import {
   presetUno,
   presetIcons,
   presetAttributify,
-  presetTypography
-} from "unocss"
-import solidJs from "@astrojs/solid-js"
+  presetTypography,
+} from "unocss";
+import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,14 +18,14 @@ export default defineConfig({
         presetUno(),
         presetTypography({
           cssExtend: {
-            ":not(pre) > code::before,:not(pre) > code::after": ""
-          }
+            ":not(pre) > code::before,:not(pre) > code::after": "",
+          },
         }),
-        presetIcons()
-      ]
+        presetIcons(),
+      ],
     }),
-    solidJs()
+    solidJs(),
   ],
   output: "server",
-  adapter: vercel()
-})
+  adapter: vercel(),
+});
